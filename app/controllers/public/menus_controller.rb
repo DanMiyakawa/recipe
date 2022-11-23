fe4bf61c6a6b3b0ce693feb1fcf01589ba3d22e4
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-11-23 01:38:13 +0000

    【add】menu

--- app/controllers/public/menus_controller.rb
+++ app/controllers/public/menus_controller.rb
@@ -1,9 +1,13 @@
 class Public::MenusController < ApplicationController
-  
+
   def new
+    @menu = Menu.new
   end
-  
+
   def create
+    @menu = Menu.new(menu_params)
+    @menu.save
+    redirect_to menu_path(@menu.id)
   end
 
   def index
@@ -11,12 +15,21 @@ class Public::MenusController < ApplicationController
   end
 
   def show
+    @menu = Menu.find(params[:id])
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:user_id, :name, :introduction, :genre_id, :recipe_image, :status,
+    ingredients_attributes:[:id, :name, :amount, :_destroy],
+    steps_attributes:[:id, :number, :explanation, :_destroy])
+  end
+
 end
